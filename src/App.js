e36478f59e9a4c5b9dc5ff25ef944773d432f36e
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-09-23 22:05:05 +0300

    navbar finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Navbar from "./components/Navbar"
+
 function App() {
-  return <div className="App">Hello World!</div>
+  return <div className="App">
+    <Navbar />
+    <input />
+  </div>
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { MdOutlineMenu } from 'react-icons/md'
+
+const Navbar = () => {
+  const [toggleBar, setToggleBar] = useState(false)
+
+  return (
+    <>
+      <div className="bg-[#212529] flex justify-between sm:justify-around text-white items-center py-3">
+        <div className="text-xl ml-5 sm:ml-0">Alpha Tech</div>
+        <div>
+          <div
+            className="sm:hidden mr-5 p-2 text-3xl cursor-pointer"
+            onClick={() => setToggleBar(!toggleBar)}
+          >
+            <MdOutlineMenu />
+          </div>
+          <ul className="sm:flex hidden">
+            {['Service', 'Portfolio', 'About Us', 'Staff', 'Contact Us'].map(
+              (item, idx) => (
+                <li
+                  key={idx}
+                  className="p-2 text-gray-400 hover:text-gray-300 duration-300 "
+                >
+                  <a href="!#"> {item} </a>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      </div>
+      {toggleBar && (
+        <div className='bg-[#212529] pb-2 sm:hidden'>
+          <ul className="ml-[.7rem]">
+            {['Service', 'Portfolio', 'About Us', 'Staff', 'Contact Us'].map(
+              (item, idx) => (
+                <li
+                  key={idx}
+                  className="p-2 text-gray-400 hover:text-gray-300 duration-300 "
+                >
+                  <a href="!#"> {item} </a>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Navbar
